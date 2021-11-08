f6d9f0a526ed1549b4d5524ecfe248a35b0a0738
soyul <thduf0623@likelion.org>
2021-11-09 03:49:34 +0900

    배포fix

--- config/settings.py
+++ config/settings.py
@@ -73,16 +73,30 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
+import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd229kgn26mvd9k',
+        'USER': 'jrhwynpvpfygfh',
+        'PASSWORD': '71aaf8a59eea2918772ee88dad4b3f3f47a14ae3b3a31cd9c778050bc135f05a',
+        'HOST': 'ec2-34-203-182-172.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
